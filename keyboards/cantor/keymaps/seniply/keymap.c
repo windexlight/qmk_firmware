5ef4358b61b6ac94469ac8f037add31785429ed2
windexlight <windexlight@gmail.com>
2025-07-21 20:43:35 -0700

    Fixes and tweaks

--- keyboards/cantor/keymaps/seniply/keymap.c
+++ keyboards/cantor/keymaps/seniply/keymap.c
@@ -43,10 +43,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *                       └───┘   └───┘
       */
     [_COLEMAK_DH] = LAYOUT_split_3x6_3(
-        KC_ESC,   KC_Q,  KC_W,  KC_F,  KC_P,  KC_B,      KC_J,  KC_J,  KC_U,    KC_Y,   KC_QUOT, KC_BSPC,
+        KC_ESC,   KC_Q,  KC_W,  KC_F,  KC_P,  KC_B,      KC_J,  KC_L,  KC_U,    KC_Y,   KC_QUOT, KC_BSPC,
         KC_LALT,  KC_A,  KC_R,  KC_S,  KC_T,  KC_G,      KC_M,  KC_N,  KC_E,    KC_I,   KC_O,    KC_ENT,
         KC_LCTL,  KC_Z,  KC_X,  KC_C,  KC_D,  KC_V,      KC_K,  KC_H,  KC_COMM, KC_DOT, KC_SLSH, KC_DEL,
-                        MO(_EXT), KC_LSFT, MO(_SYM),    MO(_NUM), KC_SPC, MO(_FNC)
+                        MO(_EXT), KC_LSFT, MO(_NUM),    MO(_SYM), KC_SPC, MO(_FNC)
     ),
     [_EXT] = LAYOUT_split_3x6_3(
         KC_TRNS, KC_NO, _BAK,  _FND,  _FWD,  KC_INS,   KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_CAPS, KC_TRNS,
@@ -64,13 +64,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_EXLM, KC_AT, KC_HASH, KC_DLR,  KC_PERC,      KC_EQL,  KC_GRV,  KC_COLN, KC_SCLN, KC_PLUS, KC_TRNS,
         KC_TRNS, _LALT,   _LGUI, _LSFT,   _LCTL,   KC_CIRC,      KC_ASTR, KC_LPRN, KC_LCBR, KC_LBRC, KC_MINS, KC_TRNS,
         KC_TRNS, KC_NO,   KC_NO, KC_BSLS, KC_PIPE, KC_AMPR,      KC_TILD, KC_RPRN, KC_RCBR, KC_RBRC, KC_UNDS, KC_TRNS,
-                                    KC_NO, KC_TRNS, KC_TRNS,    KC_NO, KC_TRNS, KC_NO
+                                      KC_NO, KC_TRNS, KC_NO,    KC_TRNS, KC_TRNS, KC_NO
     ),
     [_NUM] = LAYOUT_split_3x6_3(
         KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NUM,      KC_EQL,  KC_7, KC_8, KC_9, KC_PLUS, KC_TRNS,
         KC_TRNS, _LALT, _LGUI, _LSFT, _LCTL,  _RALT,      KC_ASTR, KC_4, KC_5, KC_6, KC_MINS, KC_TRNS,
         KC_TRNS, KC_NO, KC_NO, KC_TAB, KC_NO, KC_NO,      KC_0,    KC_1, KC_2, KC_3, KC_SLSH, KC_TRNS,
-                               KC_NO, KC_TRNS, KC_NO,    KC_TRNS, KC_TRNS, KC_NO
+                             KC_NO, KC_TRNS, KC_TRNS,    KC_NO, KC_TRNS, KC_DOT
     )
 };
 
